15a30f08c88d74d8c1eb31ffc670938ce93eaa91
Joe Bacal <jebacal@smith.edu>
2016-01-29 16:45:22 -0500

    removes extra uneeded file

--- translation_studies_cats.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-// register_activation_hook( __FILE__, 'translation_categories_on_activate' );
-// function translation_categories_on_activate() {
-//   wp_create_category('CategoryNameHere');
-// }
